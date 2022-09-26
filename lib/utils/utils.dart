aad2918c616be30638ef7cc07c39372125c01be5
Gianluca Cornetta <gianluca.cornetta@gmail.com>
2022-09-26 08:40:47 +0200

    Updated utils.dart to work with Flutter 3.3.2

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,20 +1,20 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Command {
   static final all = [hello];
-
   static const hello = 'hello car';
 }
 
 class Utils {
-  static void scanText(String rawText, String apiUrl) {
+  static void scanText(String rawText) {
     final text = rawText.toLowerCase();
     if (text.contains(Command.hello)) {
       final cmd = _getTextAfterCommand(text: text, command: Command.hello);
 
       if (cmd == 'park' || cmd == 'stop') {
-        _sendCommand(cmd, apiUrl);
+        _sendCommand(cmd);
       } else {
 
       }
@@ -22,24 +22,24 @@ class Utils {
   }
 
   static String _getTextAfterCommand({
-    required String text,
-    required String command,
+    @required String text,
+    @required String command,
   }) {
     final indexCommand = text.indexOf(command);
     final indexAfter = indexCommand + command.length;
 
     if (indexCommand == -1) {
-      return '';
+      return null;
     } else {
       return text.substring(indexAfter).trim();
     }
   }
 
-  static Future _sendCommand(String cmd, String apiUrl) async {
+  static Future _sendCommand(String cmd) async {
     var client = http.Client();
     try {
       var response = await client.post(
-        Uri.http(apiUrl, '/command'),
+        Uri.http('http://192.168.100.1:6000', '/command'),
         headers: {"Content-Type": "application/json", "Connection": "keep-alive"},
         body: json.encode({'command': cmd }));
       if (response.statusCode == 200) { 
